507064c4d4ce4db9315828cb5535bd91f7c43aa3
allanizzo <allan.izzo@gmail.com>
2015-04-16 17:31:34 -0400

    structure going in place

--- 35_circular_primes.rb
+++ 35_circular_primes.rb
@@ -11,4 +11,39 @@
 
 # How many circular primes are there below one million?
 
+def is_prime(num)
+	count = 2
+	while count < num
+		if num%count != 0
+			return false
+		end
+		count += 1
+	end
+	return true
+end
+
+
+def prime_list_creator(maxx)
+	prime_list = []
+	count = 2
+	while count <= maxx
+		if is_prime(count) == true
+			prime_list << count
+		end
+		count += 1
+	end
+	# creates list of primes up to max loop (in this case 1,000,000)
+end
+
+def is_circular(num)
+
+
+end
+
+def looper()
+	max = 1000000
+# calls is_circular
+
+end 
+
 
