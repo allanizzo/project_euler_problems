b21713dd4b24562c9f3c821491e2c327aa401580
allan.izzo <allan.izzo@gmail.com>
2015-05-13 18:40:03 -0400

    solved, calcite<10s, low priority revisit

--- 40_champernownes_constant.rb
+++ 40_champernownes_constant.rb
@@ -14,3 +14,39 @@
 # find the value of the following expression.
 
 # d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+
+
+def champernownernumcreator()
+	max = 1000000
+	num_string = ""
+	count = 1
+	while count <= max
+		num_string << count.to_s
+		count += 1
+	end
+	return num_string
+end
+# p champernownernumcreator()
+
+def indexer()
+	num_list = []
+	num_string = champernownernumcreator
+	index = 1
+	while index <= 1000000
+		num_list << num_string[index-1]
+		index = index*10
+	end
+	p "num_list is #{num_list}"
+	product = 1
+	num_list.each do |mult|
+		product = product*mult.to_i
+	end
+	return product
+end
+
+
+p indexer
+
+
+
+
