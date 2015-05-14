6d15cdc9f082c0505c6ae0405783538efb0d5390
allan.izzo <allan.izzo@gmail.com>
2015-05-14 13:58:48 -0400

    solved, calcite<1min, low priority revisit

--- 43_sub_string_divisibility.rb
+++ 43_sub_string_divisibility.rb
@@ -21,3 +21,77 @@
 
 # Find the sum of all 0 to 9 pandigital numbers
 # with this property.
+
+def prime_dict()
+	list_of_primes = [2,3,5,7,11,13,17]
+	prime_dict = {}
+	(3..9).each do |p|
+		prime_dict[p] = list_of_primes[p-3]
+	end
+	return prime_dict
+end
+
+def perm_checker(list, dictionary)
+	# p dictionary
+	index_lo = 1 # 2nd digit
+	index_hi = 3 # 4th digit
+	while index_hi <=9
+		# p list[index_lo..index_hi].join().to_i
+		if list[index_lo..index_hi].join().to_i % dictionary[index_hi] != 0
+			return false
+		end
+		index_lo += 1
+		index_hi += 1
+	end
+	return true
+end
+
+# p perm_checker([1,4,0,6,3,5,7,2,8,9],prime_dict())
+
+
+def permuterizer()
+	prime_dict = prime_dict()
+	perm = [0,1,2,3,4,5,6,7,8,9]
+	good_perms = []
+	list_of_perms = perm.permutation(10).to_a
+	# p list_of_perms
+	# list_of_perms will have all combinations of 0-9 pandigitals
+	list_of_perms.each do |perm1|
+		# p "perm1 is #{perm1}"
+		# perm1.each do |perm2|
+			# p "perm2 is #{perm2}"
+			if perm_checker(perm1, prime_dict) == true
+				good_perms << perm1.join().to_i
+			end
+		# end
+	end
+	# return good_perms
+	sum = 0
+	good_perms.each do |ans|
+		sum = sum + ans
+	end
+	return sum
+end
+
+p permuterizer
+
+# def summer()
+# 	list_of_answers = permuterizer
+# 	sum = 0
+# 	list_of_answers.each do |ans|
+# 		sum += ans
+# 	end
+# 	p "sum is #{sum}"
+# 	return sum
+# end
+
+# p summer
+
+
+
+
+
+
+
+
+
