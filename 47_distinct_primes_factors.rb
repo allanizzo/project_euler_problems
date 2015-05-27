1778c70d9972e194023faac118fe1a74c4a81f4f
allanizzo <allan.izzo@gmail.com>
2015-05-26 22:09:22 -0400

    3 consec works, 4 slow but running

--- 47_distinct_primes_factors.rb
+++ 47_distinct_primes_factors.rb
@@ -54,7 +54,7 @@ end
 def prime_number_less_than_list_creator(num)
 	less_than_prime_list = []
 	potential_less_than_prime = 2
-	while potential_less_than_prime < num
+	while potential_less_than_prime < num/6
 		if is_prime(potential_less_than_prime) == true
 			less_than_prime_list << potential_less_than_prime
 		end
@@ -63,64 +63,89 @@ def prime_number_less_than_list_creator(num)
 	return less_than_prime_list
 end
 
+# p prime_number_less_than_list_creator(9999)
+
 # p prime_number_less_than_list_creator(100)
 
 
 def arr_mult(arr, num)
-	# so incoming is an array with 4 prime factors
-	# we need to check to see if any combo works
-	# as well as any combos with exponents
-	# p "arr is #{arr}"
+
 	arr.each do |mini_arr|
 		# p "mini_arr is #{mini_arr}"
-		product = mini_arr[0]*mini_arr[1]*mini_arr[2]*mini_arr[3]
-		if product == num
+		product1 = mini_arr[0]*mini_arr[1]*mini_arr[2]*mini_arr[3]
+		product2 = (mini_arr[0]**2)*mini_arr[1]*mini_arr[2]*mini_arr[3]
+		product3 = (mini_arr[1]**2)*mini_arr[0]*mini_arr[2]*mini_arr[3]
+		product4 = (mini_arr[2]**2)*mini_arr[1]*mini_arr[0]*mini_arr[3]
+		product5 = (mini_arr[3]**2)*mini_arr[1]*mini_arr[0]*mini_arr[2]
+		if product1 == num
+			p "product1 is #{product1}"
+			p "mini_arr is #{mini_arr}"
 			return true
+		elsif product2 == num
+			"product2 is #{product2}"
+			p "mini_arr is #{mini_arr}"
+			return true
+		elsif product3 == num
+			"product3 is #{product3}"
+			p "mini_arr is #{mini_arr}"
+			return true
+		elsif product4 == num
+			"product4 is #{product4}"
+			p "mini_arr is #{mini_arr}"
+			return true
+		elsif product5 == num
+			"product5 is #{product5}"
+			p "mini_arr is #{mini_arr}"
+			return true			
+				
 		end
-
 	end
 	return false
 end
 
 
 
-def consec_4_prime_check(num_1,num_2,num_3,num_4, hash)
-	# each number coming in and has a key and the values are lists of primes smaller than it
-	# p "hash is #{hash}"
-	# p "num_1 is #{num_1}"
-	primes1 = hash[num_1].combination(4).to_a
+def consec_4_prime_check(num_1,num_2,num_3, num_4, hash)
+	primes1 = hash[num_1]#.combination(4).to_a
 	if arr_mult(primes1,num_1) == true
-		primes2 = hash[num_2].combination(4).to_a
+		primes2 = hash[num_2]#.combination(4).to_a
 		if arr_mult(primes2,num_2) == true
-			primes3 = hash[num_3].combination(4).to_a
+			primes3 = hash[num_3]#.combination(4).to_a
 			if arr_mult(primes3,num_3) == true
-				primes4 = hash[num_4].combination(4).to_a
+				# return true
+				primes4 = hash[num_4]#.combination(4).to_a
 				if arr_mult(primes4,num_4) == true
 					return true
 				end
 			end
-		end
-	else
-		return false
+		end	
 	end
+	return false
 end
-
-
 
 def num_runner()
 	start_time = Time.new
-	num_1 = 10
+	num_1 = 1000
 	num_2 = num_1+1
 	num_3 = num_2+1
 	num_4 = num_3+1
+	p "go prime_factorhash"
 	prime_factorhash = {num_1 => prime_number_less_than_list_creator(num_1),
 		num_2 => prime_number_less_than_list_creator(num_2),
 		num_3 => prime_number_less_than_list_creator(num_3),
 		num_4 => prime_number_less_than_list_creator(num_4)
 	}
+
+	combo_factorhash = {num_1 => prime_factorhash[num_1].combination(4).to_a,
+	num_2 => prime_factorhash[num_2].combination(4).to_a,
+	num_3 => prime_factorhash[num_3].combination(4).to_a,
+	num_4 => prime_factorhash[num_4].combination(4).to_a
+	}
+
+	p "end prime_factorhash"
 	# p prime_factorhash
-	while true
-		if consec_4_prime_check(num_1,num_2,num_3,num_4, prime_factorhash) == true
+	while num_4 <= 9999
+		if consec_4_prime_check(num_1,num_2,num_3,num_4, combo_factorhash) == true
 			end_time = Time.new # may need to be moved up
 			return [num_1,num_2,num_3,num_4,"calctime is #{end_time-start_time}"]
 		end
@@ -129,16 +154,12 @@ def num_runner()
 		num_2 += 1
 		num_3 += 1
 		num_4 += 1
+		p "num_1 is #{num_1}"
 
-		# prime_factorhash[num_1] = prime_factorhash[num_2]
-		# prime_factorhash[num_2] = prime_factorhash[num_3]
-		# prime_factorhash[num_3] = prime_factorhash[num_4]
-		# prime_factorhash[num_4] = prime_number_less_than_list_creator(num_4)
-		prime_factorhash = {num_1 => prime_number_less_than_list_creator(num_1),
-		num_2 => prime_number_less_than_list_creator(num_2),
-		num_3 => prime_number_less_than_list_creator(num_3),
-		num_4 => prime_number_less_than_list_creator(num_4)
-			}
+		prime_factorhash[num_4] = prime_number_less_than_list_creator(num_4)
+		prime_factorhash.delete((num_1-1))
+		combo_factorhash[num_4] = prime_factorhash[num_4].combination(4).to_a
+		combo_factorhash.delete((num_1-1))
 
 	end
 
@@ -153,6 +174,34 @@ p num_runner()
 # ********************************************************************************  #
 # ********************************************************************************  #
 # ********************************************************************************  #
+
+# def consec_4_prime_check(num_1,num_2,num_3, num_4, hash)
+# 	# each number coming in and has a key and the values are lists of primes smaller than it
+# 	# p "hash is #{hash}"
+# 	# p "num_1 is #{num_1}"
+# 	primes1 = hash[num_1].combination(4).to_a
+
+# 	so the new incoming hash would simply be
+# 	primes1 = newhash[num_1]
+
+# 	if arr_mult(primes1,num_1) == true
+# 		primes2 = hash[num_2].combination(4).to_a
+# 		if arr_mult(primes2,num_2) == true
+# 			primes3 = hash[num_3].combination(4).to_a
+# 			if arr_mult(primes3,num_3) == true
+# 				# return true
+# 				primes4 = hash[num_4].combination(4).to_a
+# 				if arr_mult(primes4,num_4) == true
+# 					return true
+# 				end
+# 			end
+# 		end
+# 	else
+# 		return false
+# 	end
+# end
+
+
 
 
 
